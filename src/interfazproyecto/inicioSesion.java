d54c0e83fdc7526036f7b5f90b2eb30e51fa6e2e
Marcos F3rnando Morales <125113310+Marcos-F-Morales@users.noreply.github.com>
2023-10-29 18:33:45 -0600

    change inicio sesion

--- src/interfazproyecto/inicioSesion.java
+++ src/interfazproyecto/inicioSesion.java
@@ -11,7 +11,14 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.*;
 import java.awt.*;
+import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import org.json.JSONArray;
+import org.json.JSONObject;
 /**
  *
  * @author pablo
@@ -251,9 +258,55 @@ public class inicioSesion extends javax.swing.JFrame {
     }//GEN-LAST:event_InicioSesionBtmMousePressed
 
     private void InicioSesionBtmActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InicioSesionBtmActionPerformed
-    Seleccion abrir = new Seleccion();
-    abrir.setVisible(true);
-    this.setVisible(false);
+   //Seleccion abrir = new Seleccion();
+    //abrir.setVisible(true);
+    //this.setVisible(false);
+    try {
+        // URL de la API de Django para la autenticación de usuarios
+        String apiUrl = "http://127.0.0.1:8000/usuario/";
+
+        // Datos de inicio de sesión
+        String usuario = usuario_text.getText();
+        String contrasena = String.valueOf(contrasena_text.getPassword());
+
+        // Crear la URL
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        // Configurar la conexión para enviar datos
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoOutput(true);
+
+        // Crear el JSON con los datos de inicio de sesión
+        JSONObject jsonInput = new JSONObject();
+        jsonInput.put("usuario", usuario);
+        jsonInput.put("contrasena", contrasena);
+
+        // Enviar los datos como JSON
+        OutputStream os = connection.getOutputStream();
+        os.write(jsonInput.toString().getBytes());
+        os.flush();
+        os.close();
+
+        // Obtener la respuesta de la API
+        int responseCode = connection.getResponseCode();
+        if (responseCode == 200) {
+            // Inicio de sesión exitoso, redirigir a la siguiente ventana
+             Seleccion abrir = new Seleccion();
+            abrir.setVisible(true);
+            this.setVisible(false);
+        } else {
+            // Inicio de sesión fallido, mostrar un mensaje de error
+            JOptionPane.showMessageDialog(this, "Verifique los datos de inicio de sesión", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+    } catch (IOException e) {
+        e.printStackTrace();
+        // Manejo de errores de conexión o lectura de respuesta
+        JOptionPane.showMessageDialog(this, "Error de conexión", "Error", JOptionPane.ERROR_MESSAGE);
+    }
+    
     }//GEN-LAST:event_InicioSesionBtmActionPerformed
 
     private void RegistrarBtmActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RegistrarBtmActionPerformed
